0b23c4c04fc6b931f98aa6fdb70973eaa9d3f0cc
johnnybenson-test <64909968+johnnybenson-test@users.noreply.github.com>
2020-05-20 14:46:04 -0400

    Update metadata

--- config/metadata.js
+++ config/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: 'The Coop',
-  description: 'Farm fresh eggs from coop to stoop',
-  twitter: '@TheCoop',
-  keywords: ['The Coop', 'Chickens', 'Eggs'],
+  title: 'Pizza Hut',
+  description: 'We make pizzas, not websites',
+  twitter: '@HumanVentures',
+  keywords: ['Pizza', 'Cheese'],
   lang: 'en',
   image: {
     src: '/images/site.png', // Path to your image you placed in the 'static' folder
